35faaf652f888a53b1f08e583bd2ca69d29a5c5e
fatmaelkassaby2003 <125102138+fatmaelkassaby2003@users.noreply.github.com>
2024-10-24 07:16:53 +0300

    Add files via upload

---
+++ task12/BLOG/config/conn.php
@@ -0,0 +1,7 @@
+<?php
+include './config/env.php';
+
+$conn = new PDO(dsn: "mysql:host=".DB_HOST.";dbname=".DB_NAME, username:DB_USER, password:DB_PASSWORD);
+var_dump(value: $conn);
+die();
+
---
+++ task12/BLOG/config/env.php
@@ -0,0 +1,7 @@
+<?php 
+        define(constant_name: 'DB_HOST',value: 'localhost');
+        define(constant_name:'DB_NAME',value: 'blog');
+        define(constant_name:'DB_USER',value: 'root');
+        define(constant_name:'DB_PASSWORD',value: '');
+
+?>
